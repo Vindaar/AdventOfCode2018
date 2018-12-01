0fbd854e031d6b9e8ea864ec3f1a73252315139f
Sebastian Schmidt <basti90@gmail.com>
2018-12-01 06:27:41 +0100

    add solution to day 1

---
+++ day1/day1.nim
@@ -0,0 +1,46 @@
+import sequtils, strutils, os
+import zero_functional, sets
+
+const test1 = """+3
++3
++4
+-2
+-4
+"""
+
+proc dostuff(file: seq[string]) =
+  var freq = 0
+  var fSet = initSet[int]()
+  var p1Res = 0
+  var p1found = false
+  var i = 0  
+  while true:
+    let l = file[i]
+    if l.len > 0:
+      let f = l.strip(chars = Whitespace).parseInt
+      freq += f
+      if freq in fSet:
+        echo "Found f ", freq
+        break
+      else:
+        fSet.incl freq
+    inc i
+    if i == file.len:
+      if not p1Found:
+        p1Res = freq
+      p1Found = true
+      i = 0
+      
+  echo "res freq p1 ", p1Res
+  echo "res freq p2 ", freq
+
+proc main =
+
+  let file = readFile("day1Data.txt").splitLines
+
+  file.dostuff
+
+  test1.splitLines().dostuff  
+  
+when isMainModule:
+  main()
